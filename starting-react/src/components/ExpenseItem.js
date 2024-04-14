68537d1e793bda1300d261c7e3367ce91faf449d
shubham <shubhamchavan71195@gmail.com>
2024-04-14 17:31:18 +0530

    Added date format as calender

--- starting-react/src/components/ExpenseItem.js
+++ starting-react/src/components/ExpenseItem.js
@@ -1,17 +1,35 @@
 import './ExpenseItem.css'
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 function ExpenseItem(props){
-    // const expenseDate=new Date(2023,7,15).toISOString();
-    // const expenseName='Insurance';
-    // const expenseAmt=50;
-    // const expenseLocation='Bangalore';
+    const month=months[props.date.getMonth()];
+    const year=props.date.getFullYear();
+    const date=props.date.getDate();
     return(
     <div className='expense-item'>
-    <div>{props.date.toISOString()}</div>
-    <div className='expense-item__price'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__description'>${props.price}</div>
+    {/* <div>{props.date.toISOString()}</div> */}
+    <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{date}</div>
     </div>
-    <div className='expense-item__location'>Location
+    <div className='expense-item__description-item__price'>
+        <h2>{props.title}</h2>
+        <div className=''>${props.price}</div>
+    </div>
+    <div className='expense-item__price'>Location
         <h2>{props.location}</h2>
     </div>
     </div>
